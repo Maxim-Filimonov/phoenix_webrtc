c5be036a68b104860075b70a13547f3111f4ce63
Maxim Filimonov <tpaktopsp@gmail.com>
2021-08-07 17:12:23 +0800

    add setup of stream

--- assets/js/app.js
+++ assets/js/app.js
@@ -33,3 +33,44 @@ const reportError = (where) => (error) => {
 function log() {
   console.log(...arguments);
 }
+
+function setVideoStream(videoElement, stream) {
+  videoElement.srcObject = stream;
+}
+
+function unsetVideoStream(videoElement) {
+  if (videoElement.srcObject) {
+    // stop tracks before disconnecting
+    videoElement.srcObject.getTracks().forEach((track) => track.stop());
+  }
+  videoElement.removeAttribute("src");
+  videoElement.removeAttribute("srcObject");
+}
+
+async function connect() {
+  connectButton.disabled = true;
+  disconnectButton.disabled = false;
+  callButton.disabled = false;
+
+  try {
+    const localStream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+      video: true,
+    });
+
+    setVideoStream(localVideo, localStream);
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function disconnect() {
+  connectButton.disabled = false;
+  disconnectButton.disabled = true;
+  callButton.disabled = true;
+  unsetVideoStream(localVideo);
+  unsetVideoStream(remoteVideo);
+  remoteStream = new MediaStream();
+
+  setVideoStream(remoteVideo, remoteStream);
+}
